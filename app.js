cb28520cbaff767ad3873fb99253c582be561452
Antonio656 <51298456+Antonio656@users.noreply.github.com>
2021-09-05 17:10:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,93 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-}
-// hamburger menu//
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const input = document.querySelector('input');
+const shorten = document.querySelector('.shorten');
+const lists = document.querySelector('.lists');
 
-hamburger.addEventListener("click", mobileMenu);
+shorten.addEventListener('click', shortenLink);
 
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
 
-const navLink = document.querySelectorAll(".nav-link");
+function shortenLink(e) {
+  const link = input.value;
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+  if (link === '') {
+    input.style.border = '1px solid red';
+    input.classList.add('red');
+    alert("Insert link");
+  } else {
+    input.classList.remove('red');
+    input.style.border = 'unset';
 
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}
+    fetch('https://api-ssl.bitly.com/v4/shorten', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer f9328cfa962461517d946be606d00f71fd34dd32',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "long_url": link,
+        "domain": "bit.ly"
+      })
+    }).then(function (res) {
+      return res.json();
+    }).then(function (data) {
+      // console.log(data);
+      createLink(data.id, link);
+    }).catch(function (err) {
+      console.log(err);
+
+    })
+  }
+
+  function createLink(line, mylink) {
+    // const link = input.value;
+
+    const listDiv = document.createElement('div');
+    listDiv.classList.add('list');
+
+    const long = document.createElement('div');
+    long.classList.add('long-link')
+    long.textContent = mylink;
+    listDiv.appendChild(long)
+    // add longlink to local storage
+    // saveMyLinks(mylink);
+
+    const short = document.createElement('p');
+    short.classList.add('short-link')
+    short.innerText = line;
+    listDiv.appendChild(short);
+    //add shortlink to local storage
+    // saveMyLinks(mylink, line);
+
+    const btn = document.createElement('button')
+    btn.classList.add('copy-link')
+    btn.innerText = 'Copy';
+    listDiv.appendChild(btn);
+
+    lists.appendChild(listDiv)
+  }
+   }
+
+   
+
+  //hamburger menu animation
+  function myFunction(x) {
+    x.classList.toggle("change");
+  }
+  // hamburger menu//
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", mobileMenu);
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
+  const navLink = document.querySelectorAll(".nav-link");
+
+  navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
